0f7826a5efdeb966b3439b27d56e1a4a14968d5d
imrany <imranmat254@gmail.com>
2023-03-07 04:16:55 +0300

    added the callback route

--- router/api.js
+++ router/api.js
@@ -6,4 +6,7 @@ const auth=require('../controller/auth')
 router.get('/password',auth.mpesaPassword);
 //stk push route
 router.post('/stk/push',auth.token,auth.stkPush);
+// callback route
+router.post('/callback',auth.callBack);
+
 module.exports=router;
