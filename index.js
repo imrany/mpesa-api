a239925d027c7fb74fbff17bb9a96bb9c57890e1
imrany <imranmat254@gmail.com>
2023-03-07 04:16:03 +0300

    connected index.js to mongodb

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const bodyParser=require('body-parser');
+const cors=require('cors');
 require('dotenv').config();
-const bodyParser=require('body-parser')
-
-//connect to momgodb
-/*mongoose.connect(process.env.DATABASE,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>{
-    //listening to server
-    const port=process.env.PORT||3000;
-    app.listen(port,()=>{
-        console.log(`Server opening at Port ${port}`)
-    })
-}).catch(err=>console.log(err));
-mongoose.Promise=global.Promise*/
+const app = express();
  
 //bodyparser middleware
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 //router middleware
 app.use('/api', require('./router/api'))
  
-//listening to server
-const port=process.env.PORT||3000;
-app.listen(port,()=>{
-    console.log(`Server opening at Port ${port}`)
-})
+//connect to momgodb
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    //listening to server
+    const port=process.env.PORT||5000;
+    app.listen(port,()=>{
+        console.log(`Server opening at Port ${port}`)
+    })
+}).catch(err=>console.log(err));
+mongoose.Promise=global.Promise
 
